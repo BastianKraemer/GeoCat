4aec6f1f67dfd6f15f35e9b02898ec8343dfbad6
Bastian Kraemer <Bastian.Kraemer@akubix.de>
2015-11-24 11:55:49 +0100

    Update Grunt/NPM configuration to support JSDoc

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,18 +3,30 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
     propertiesToJSON: {
         main: {
             src: 'src/locale/de.properties',
             dest: 'src/locale'
+        }
+    },
+
+    jsdoc: {
+        dist : {
+            src: ['src/js/*.js'],
+            dest: 'doc'
         }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-properties-to-json');
+  grunt.loadNpmTasks('grunt-jsdoc');
 
   // Default task(s).
   grunt.registerTask('default', ['propertiesToJSON']);
+  grunt.registerTask('translate', ['propertiesToJSON']);
+  grunt.registerTask('doc', ['jsdoc']);
+  grunt.registerTask('build', ['jsdoc', 'propertiesToJSON']);
 
 };

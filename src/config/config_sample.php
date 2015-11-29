295d6fc7cb877e63672c3958b499de240a734f31
Bastian Kraemer <Bastian.Kraemer@akubix.de>
2015-11-29 15:59:39 +0100

    Append schema name entry to sample configuration

--- src/config/config_sample.php
+++ src/config/config_sample.php
@@ -6,6 +6,7 @@
 			"database.port" => "3306",
 			"database.type" => "mysql", 	/* For all supported databases see https://secure.php.net/manual/en/pdo.drivers.php */
 			"database.username" => "root",
-			"database.password" => ""
+			"database.password" => "",
+			"database.schema" => "geocat"
 	];
 ?>

2f6d65c7014a61b35857442921ec67a9b131e148
Bastian Kraemer <Bastian.Kraemer@akubix.de>
2016-01-21 18:50:44 +0100

    Update translation file

--- src/views/Page_Login.php
+++ src/views/Page_Login.php
@@ -18,14 +18,14 @@
 
 			<div role="main" class="ui-content">
 				<form id="form-login" action="./query/login.php" method="POST">
-					<label for="useremail"><?php $locale->write("createaccount.question.emailorusername"); ?>:</label>
-					<input type="text" id="useremail" name="useremail" value="" placeholder="<?php $locale->write('createaccount.question.emailorusername'); ?>" maxlength="50" required="required">
+					<label for="useremail"><?php $locale->write("login.email_or_username"); ?>:</label>
+					<input type="text" id="useremail" name="useremail" value="" placeholder="<?php $locale->write('login.email_or_username'); ?>" maxlength="50" required="required">
 					<label for="userpassword"><?php $locale->write("createaccount.password"); ?>:</label>
 					<input type="password" id="userpassword" name="userpassword" value="" placeholder="<?php $locale->write('createaccount.password'); ?>" maxlength="50" autocomplete="off" required="required">
 					<!--
 					<p>
 						<input type="checkbox" id="rememberme" name="rememberme">
-						<label for="rememberme"><?php //$locale->write("createaccount.rememberme"); ?></label>
+						<label for="rememberme"><?php //$locale->write("login.rememberme"); ?></label>
 					</p>
 					-->
 					<div class="ui-grid-a ui-responsive">
@@ -33,11 +33,11 @@
 							<a id="login-back" href="#home" role="button" data-transition="fade" data-direction="reverse" class="ui-btn ui-corner-all"><?php $locale->write("back"); ?></a>
 						</div>
 						<div class="ui-block-b">
-							<input type="submit" value="<?php $locale->write('send'); ?>">
+							<input type="submit" value="<?php $locale->write('signin'); ?>">
 						</div>
 					</div>
 
-					<h2><?php $locale->write("createaccount.question.noaccount"); ?>?</h2>
+					<h3><?php $locale->write("login.noaccount"); ?>?</h3>
 					<a href="./sites/signup.php" rel="external" data-ajax="false" role="button" class="ui-btn ui-corner-all"><?php $locale->write("createaccount.confirm"); ?></a>
 				</form>
 			</div>

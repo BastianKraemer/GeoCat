6351b415025d2ce8e9227dd36c30aa87e5578252
Bastian Kraemer <Bastian.Kraemer@akubix.de>
2016-02-22 17:16:50 +0100

    Make some challenge data editable via GUI

--- src/js/challenges/ChallengeInfoController.js
+++ src/js/challenges/ChallengeInfoController.js
@@ -1,14 +1,69 @@
 function ChallengeInfoController(sessionKey){
 
 	var challengeSessionKey = sessionKey;
+	var challengeData;
+
+	// Id configuration
+
+	var infoElements ={
+			title: "#challengeinfo-title",
+			description: "#challengeinfo-description",
+			owner: "#challengeinfo-owner",
+			type: "#challengeinfo-type",
+			startTime: "#challengeinfo-start-time",
+			endTime: "#challengeinfo-end-time"
+	}
+
+	var buttons = {
+		addCache: "#challengeinfo-add-cache",
+		createTeam: "#challengeinfo-create-team",
+		start: "#challengeinfo-start",
+		leaveChallenge: "#challengeinfo-leave",
+		resetChallenge: "#challengeinfo-reset",
+		enableChallenge: "#challengeinfo-enable",
+		deleteChallenge: "#challengeinfo-delete"
+	};
+
+	var popups = {
+			editDescriptionPopup: "#challengeinfo-editdesc-popup",
+			editEtcPopup: "#challengeinfo-editetc-popup"
+	};
+
+	var inputElements = {
+			editName: "#challengeinfo-edit-name",
+			editDesc: "#challengeinfo-edit-desc",
+			editIsPublic: "#challengeinfo-edit-ispublic",
+			editType: "#challengeinfo-edit-type",
+			editStartTime: "#challengeinfo-edit-starttime",
+			editEndTime: "#challengeinfo-edit-endtime",
+			editPredefTeams: "#challengeinfo-edit-predefteams",
+			editMaxTeams: "#challengeinfo-edit-maxteams",
+			editMaxTeamMembers: "#challengeinfo-edit-maxteam-members"
+	};
+
+	var confirmButtons = {
+			editDescriptionConfirm: "#challengeinfo-editdesc-ok",
+			editEtcConfirm: "#challengeinfo-editetc-ok"
+	};
+
+	// Public functions
 
 	this.pageOpened = function(){
 		downloadChallengeInfo();
+
+		$(confirmButtons.editDescriptionConfirm).click(editDescriptionPopupSaveButtonClicked);
+		$(confirmButtons.editEtcConfirm).click(editEtcPopupSaveButtonClicked);
 	};
 
 	this.pageClosed = function(){
+		disableControls();
 
+		for(var key in confirmButtons){
+			$(confirmButtons[key].editDescriptionConfirm).unbind();
+		}
 	};
+
+	// private functions
 
 	var ajaxError = function(xhr, status, error){
 		SubstanceTheme.showNotification("<h3>Unknown error - Ajax request failed</h3><p>" + error + "</p>", 7,
@@ -22,8 +77,16 @@ function ChallengeInfoController(sessionKey){
 			data: {task: "about", challenge: challengeSessionKey},
 			cache: false,
 			success: function(response){
-				try{
-					var responseData = JSON.parse(response);
+					var responseData;
+					try{
+						responseData = JSON.parse(response);
+					}
+					catch(e){
+						SubstanceTheme.showNotification("<h3>Unable to download challenge information</h3><p>Server returned:<br>" + response + "</p>", 7,
+														$.mobile.activePage[0], "substance-red no-shadow white");
+						return;
+					}
+
 					if(responseData.status == "ok"){
 						onChallengeDataReceived(responseData);
 					}
@@ -31,11 +94,7 @@ function ChallengeInfoController(sessionKey){
 						SubstanceTheme.showNotification("<h3>" + GeoCat.locale.get("challenge.nav.error.download_info", "Unable to download challenge information") + "</h3>" +
 														"<p>" + responseData.msg + "</p>", 7, $.mobile.activePage[0], "substance-red no-shadow white");
 					}
-				}
-				catch(e){
-					SubstanceTheme.showNotification("<h3>Unable to download challenge information</h3><p>Server returned:<br>" + response + "</p>", 7,
-													$.mobile.activePage[0], "substance-red no-shadow white");
-				}
+
 			},
 			error: ajaxError
 		});
@@ -48,40 +107,49 @@ function ChallengeInfoController(sessionKey){
 			data: {task: "coord_info", challenge: challengeSessionKey},
 			cache: false,
 			success: function(response){
+				var responseData;
 				try{
-					var responseData = JSON.parse(response);
-					if(responseData.status == "ok"){
-						onCoordinateDataReceived(responseData);
-					}
-					else{
-						// The only error that could oocur ist that the challenge has not started yet...
-						$("#challengeinfo-cache-list").html("<tr><td colspan=4>" + GeoCat.locale.get("challenge.info.not_started", "The challenge has not started yet.") + "</td></tr>");
-					}
+					responseData= JSON.parse(response);
 				}
 				catch(e){
 					SubstanceTheme.showNotification("<h3>Unable to download cache positions</h3><p>Server returned:<br>" + response + "</p>", 7,
 													$.mobile.activePage[0], "substance-red no-shadow white");
+					return;
 				}
+
+				if(responseData.status == "ok"){
+					onCoordinateDataReceived(responseData);
+				}
+				else{
+					// The only error that could oocur ist that the challenge has not started yet...
+					$("#challengeinfo-cache-list").html("<tr><td colspan=4>" + GeoCat.locale.get("challenge.info.not_started", "The challenge has not started yet.") + "</td></tr>");
+				}
+
 			},
 			error: ajaxError});
 	};
 
 	var onChallengeDataReceived = function(data){
 
-		$("#challengeinfo-title").html(data["name"]);
-		$("#challengeinfo-description").html(data["description"]);
+		challengeData = data;
 
-		$("#challengeinfo-owner").html(data["owner_name"]);
-		$("#challengeinfo-type").html(data["type_name"]);
-		$("#challengeinfo-start-time").html(data["start_time"]);
-		$("#challengeinfo-end-time").html(data["end_time"] == null ? "-" : data["end_time"]);
+		// If the challenge is not enabled then the user must be the owner
+		downloadCoordData();
 
-		updateTeamList(data);
-
-		if(data["is_enabled"]){
-			downloadCoordData();
-		}
+		updateGUIWithChallengeData();
+		updateTeamList();
+		enableControls();
 	};
+
+	var updateGUIWithChallengeData = function(){
+		$(infoElements.title).html(challengeData["name"]);
+		$(infoElements.description).html(challengeData["description"]);
+
+		$(infoElements.owner).html(challengeData["owner_name"]);
+		$(infoElements.type).html(challengeData["type_name"]);
+		$(infoElements.startTime).html(challengeData["start_time"]);
+		$(infoElements.endTime).html(challengeData["end_time"] == null ? "-" : challengeData["end_time"]);
+	}
 
 	var onCoordinateDataReceived = function(data){
 
@@ -115,12 +183,12 @@ function ChallengeInfoController(sessionKey){
 		});
 	};
 
-	var updateTeamList = function(data){
+	var updateTeamList = function(){
 		$("#challengeinfo-team-list").html("");
 
-		data["team_list"].forEach(function(teamData) {
+		challengeData["team_list"].forEach(function(teamData) {
 			var teamName;
-			if(teamData.team_id == data["your_team"]){
+			if(teamData.team_id == challengeData["your_team"]){
 				teamName = "<b><i>" + teamData.name + (teamData.has_code == 1 ? "*" : "") + "</i></b>"
 			}
 			else{
@@ -131,11 +199,186 @@ function ChallengeInfoController(sessionKey){
 				"<tr>" +
 					"<td style=\"background-color: " + teamData.color + "; width: 0px;\"></td>" +
 					"<td>" + teamName + "</td>" +
-					"<td>" + teamData.member_cnt + "/" + data["max_team_members"] + "</td>" +
+					"<td>" + teamData.member_cnt + "/" + challengeData["max_team_members"] + "</td>" +
 				"</tr>");
 		});
 	};
 
+	var enableControls = function(){
+
+		disableControls();
+		if(GeoCat.loginStatus.isSignedIn == true){
+			if(userIsChallengeOwner()){
+				// The user is the owner of this challenge
+
+				if(challengeData["is_enabled"] == 1){
+					// The challenge is already enabled - caches cannot be edited anymore
+					showButton(buttons.createTeam, function(){alert("Feature 'create team' is not implemented yet.");});
+					showButton(buttons.start, function(){alert("Feature 'start' is not implemented yet.");});
+					showButton(buttons.resetChallenge, function(){alert("Feature 'reset challenge' is not implemented yet.");});
+					showButton(buttons.deleteChallenge, function(){alert("Feature 'delete challenge' is not implemented yet.");});
+				}
+				else{
+					// The challenge is not enabled yet
+					showButton(buttons.addCache, function(){alert("Feature 'add cache' is not implemented yet.");});
+					showButton(buttons.enableChallenge, function(){alert("Feature 'enable challenge' is not implemented yet.");});
+					showButton(buttons.deleteChallenge, function(){alert("Feature 'delete challenge' is not implemented yet.");});
+
+					addClickHandlerToInfoField(infoElements.startTime, handleClickOnEditEtc, true);
+					addClickHandlerToInfoField(infoElements.endTime, handleClickOnEditEtc, true);
+					addClickHandlerToInfoField(infoElements.type, handleClickOnEditEtc, true);
+				}
+
+				addClickHandlerToInfoField(infoElements.title, handleClickOnEditDescription, false);
+				addClickHandlerToInfoField(infoElements.description, handleClickOnEditDescription, false);
+			}
+			else{
+				// The user is NOT the owner of this challenge
+				if(challengeData["your_team"] == -1){
+					// The user is already part of this challenge and has coosen a teamn
+				}
+				else{
+					// The user does not have a team yet
+				}
+			}
+		}
+
+	};
+
+	var disableControls = function(){
+		for(var key in buttons){
+			$(buttons[key]).css("display", "none");
+			$(buttons[key]).unbind();
+		}
+
+		$(infoElements.startTime).parent().unbind();
+		$(infoElements.startTime).parent().removeClass("clickable")
+		$(infoElements.endTime).parent().unbind();
+		$(infoElements.endTime).parent().removeClass("clickable")
+		$(infoElements.type).parent().unbind();
+		$(infoElements.type).parent().removeClass("clickable")
+	}
+
+	var addClickHandlerToInfoField = function(id, callback, useParentElement){
+		var el = useParentElement ? $(id).parent() : $(id);
+		el.click(callback);
+		el.addClass("clickable");
+	}
+
+	var showButton = function(id, onClickHandler){
+		$(id).css("display", "inline-block");
+		$(id).click(onClickHandler);
+	};
+
+	var userIsChallengeOwner = function(){
+		return (GeoCat.loginStatus.username == challengeData["owner_name"]);
+	};
+
+
+	/*
+	 * ========================================================================
+	 *	Click handler
+	 * ========================================================================
+	 */
+
+	var handleClickOnEditDescription = function(){
+		$(inputElements.editName).val(challengeData["name"]);
+		$(inputElements.editDesc).val(challengeData["description"]);
+		$(inputElements.editIsPublic).prop('checked', (challengeData["is_public"] == 1)).checkboxradio('refresh');
+
+		$(popups.editDescriptionPopup).popup("open", {positionTo: "window", transition: "pop"});
+	};
+
+	var handleClickOnEditEtc = function(){
+
+		$(inputElements.editType).val(challengeData["type_id"]).selectmenu('refresh');
+		$(inputElements.editStartTime).val(challengeData["start_time"]);
+		$(inputElements.editEndTime).val(challengeData["end_time"] == null ? "" : challengeData["end_time"]);
+		$(inputElements.editPredefTeams).prop('checked', (challengeData["predefined_teams"] == 1)).checkboxradio('refresh');
+		$(inputElements.editMaxTeams).val(challengeData["max_teams"]).selectmenu('refresh');
+		$(inputElements.editMaxTeamMembers).val(challengeData["max_team_members"]).selectmenu('refresh');
+		$(popups.editEtcPopup).popup("open", {positionTo: "window", transition: "pop"});
+	};
+
+	/*
+	 * ========================================================================
+	 *	Popup click handler
+	 * ========================================================================
+	 */
+
+	var editDescriptionPopupSaveButtonClicked = function(){
+
+		var newName = $(inputElements.editName).val();
+		var newDesc = $(inputElements.editDesc).val();
+		var isPubic = $(inputElements.editIsPublic).is(":checked");
+
+		sendModifiedChallengeInfo({name: newName, description: newDesc, is_Public: isPubic}, popups.editDescriptionPopup);
+	}
+
+	var editEtcPopupSaveButtonClicked = function(){
+
+		var endTime = $(inputElements.editEndTime).val();
+
+		sendModifiedChallengeInfo({
+				type_id: 				$(inputElements.editType)[0].value,
+				start_time: 		$(inputElements.editStartTime).val().replace("T", " "),
+				end_time: 			endTime == "" ? null : $(inputElements.editEndTime).val().replace("T", " "),
+				predefined_teams: 	$(inputElements.editPredefTeams).is(":checked") ? 1 : 0,
+				max_teams:			$(inputElements.editMaxTeams)[0].value,
+				max_team_members:	$(inputElements.editMaxTeamMembers)[0].value,
+			},
+			popups.editEtcPopup);
+	}
+
+	/*
+	 * ========================================================================
+	 *	AJAX functions
+	 * ========================================================================
+	 */
+
+	var sendModifiedChallengeInfo = function(ajaxData, popupId){
+		ajaxData["task"] = "modify";
+		ajaxData["challenge"] = challengeSessionKey;
+
+		$.ajax({
+			type: "POST", url: "./query/challenge.php",
+			encoding: "UTF-8",
+			data: ajaxData,
+			cache: false,
+			success: function(response){
+				try{
+					var responseData = JSON.parse(response);
+					if(responseData.status == "ok"){
+						// Update the new data in the local "database"
+						for(var key in ajaxData){
+							if(key != "task" && key != "challenge"){
+								challengeData[key] = ajaxData[key];
+								if(key == "type_id"){
+									challengeData["type_name"] = ajaxData[key] == 1 ? "Capture the Flag" : "Default Challenge";
+								}
+								updateGUIWithChallengeData();
+							}
+						}
+					}
+					else{
+						showError(GeoCat.locale.get("challenge.info.error.unknown", "Error: Update of challenge data failed."), responseData.msg);
+					}
+				}
+				catch(e){
+					console.log("ERROR: " + e);
+					console.log("Server returned: " + response);
+					showError(GeoCat.locale.get("challenge.info.error.unknown", "Error: Update of challenge data failed."));
+				}
+
+				$(popupId).popup("close");
+			},
+			error: ajaxError
+		});
+	};
+
+	var showError = function(h3, p){
+		SubstanceTheme.showNotification("<h3>" + h3 + "</h3>" + (p != null ? "<p>" + p + "</p>" : ""), 7, $.mobile.activePage[0], "substance-red no-shadow white");
+	};
 }
 
 ChallengeInfoController.currentInstance = null;
--- src/views/Page_ChallengeInfo.php
+++ src/views/Page_ChallengeInfo.php
@@ -12,31 +12,22 @@
 		}
 
 		public function printContent($config, $locale, $session, $pathToRoot){
+
+			require_once(__DIR__ . "/../app/pages/InputTemplates.php");
 ?>
 	<div data-role="page" id="ChallengeInfo" data-theme="a">
 <?php self::printHeader("Wettbewerb", true, false, $config, $session); ?>
 		<div role="main" class="ui-content">
+
 			<p id="challengeinfo-title" class="title">...</p>
 			<p id="challengeinfo-description" class="description">...</p>
 
 			<div class="substance-vflexcontainer">
-				<p class="substance-flexitem1 small center"><?php $locale->write("challenge.info.owner"); ?>:<br><span id="challengeinfo-owner">???</span></p>
-				<p class="substance-flexitem1 small center"><?php $locale->write("challenge.info.type"); ?>:<br><span id="challengeinfo-type">Capture the Flag</span>
-				<p class="substance-flexitem1 small center"><?php $locale->write("challenge.info.start"); ?>:<br><span id="challengeinfo-start-time">18.02.2016 12:00 Uhr</span></p>
-				<p class="substance-flexitem1 small center"><?php $locale->write("challenge.info.end"); ?>:<br><span id="challengeinfo-end-time">18.02.2016 18:00 Uhr</span></p>
+				<p class="substance-flexitem1 small center"><?php $locale->write("challenge.info.owner"); ?>:<br><span id="challengeinfo-owner">-</span></p>
+				<p class="substance-flexitem1 small center"><?php $locale->write("challenge.info.type"); ?>:<br><span id="challengeinfo-type">-</span>
+				<p class="substance-flexitem1 small center"><?php $locale->write("challenge.info.start"); ?>:<br><span id="challengeinfo-start-time">-</span></p>
+				<p class="substance-flexitem1 small center"><?php $locale->write("challenge.info.end"); ?>:<br><span id="challengeinfo-end-time">-</span></p>
 			</div>
-			<!-- <table style="border-spacing: 4px 0px;" class="center">
-				<tbody>
-					<tr>
-						<td><p><?php $locale->write("challenge.info.owner"); ?>:<br><span id="ChallengeInfo-owner">???</span></p></td>
-						<td><p><?php $locale->write("challenge.info.type"); ?>:<br><span id="ChallengeInfo-type">Capture the Flag</span></p></td>
-					</tr>
-					<tr>
-						<td><p><?php $locale->write("challenge.info.start"); ?>:<br><span id="ChallengeInfo-start-time">18.02.2016 12:00 Uhr</span></p></td>
-						<td><p><?php $locale->write("challenge.info.end"); ?>:<br><span id="ChallengeInfo-end-time">18.02.2016 18:00 Uhr</span></p></td>
-					</tr>
-				</tbody>
-			</table>-->
 
 			<div class="substance-vflexcontainer">
 				<div class="substance-container substance-flexitem2" style="min-width: 300px; min-height: 200px; max-height: 400px; overflow-y: auto;">
@@ -71,16 +62,81 @@
 						</tbody>
 					</table>
 				</div>
-				<!--
-					-> Team erstellen
-					-> Challenge verlassen
-					-> Go
-					-> Challenge freigeben (nur owner)
-					-> Challenge löschen (nur owner)
-				 -->
 
 				<div class="substance-footer" id="challengeinfo-footer">
+					<span id="challengeinfo-add-cache" class="substance-button substance-button-animated substance-lime" style="display: none; background-image: url('./img/plus.png'"></span>
+					<span id="challengeinfo-create-team" class="substance-button substance-button-animated substance-green" style="display: none; background-image: url('./img/plus.png'"></span>
+					<span id="challengeinfo-start" class="substance-button substance-button-animated substance-blue" style="display: none; background-image: url('./img/pin.png'"></span>
+					<span id="challengeinfo-leave" class="substance-button substance-button-animated substance-orange" style="display: none; background-image: url('./img/leave.png'"></span>
+					<span id="challengeinfo-reset" class="substance-button substance-button-animated substance-purple" style="display: none; background-image: url('./img/reset.png'"></span>
+					<span id="challengeinfo-enable" class="substance-button substance-button-animated substance-blue" style="display: none; background-image: url('./img/flag.png'"></span>
+					<span id="challengeinfo-delete" class="substance-button substance-button-animated substance-red" style="display: none; background-image: url('./img/delete.png'"></span>
 				</div>
+			</div>
+		</div>
+
+		<!-- Popup: Modify challenge name and description -->
+		<div id="challengeinfo-editdesc-popup" data-role="popup" data-theme="a" data-position-to="window" class="ui-corner-all">
+			<div data-role="header" data-theme="b">
+				<h3><?php $locale->write("challenge.info.edit.title"); ?></h3>
+			</div>
+
+			<div role="main" class="ui-content">
+
+<?php
+					InputTemplates::printTextField("challengeinfo-edit-name", false, $locale->get("challenge.create.name"), false, 63);
+					InputTemplates::printTextArea("challengeinfo-edit-desc", false, $locale->get("challenge.create.desc"), false, 512);
+?>
+
+				<label>
+			        <input id="challengeinfo-edit-ispublic" type="checkbox"><?php $locale->write("challenge.create.public"); ?>
+			    </label>
+				<hr>
+				<button id="challengeinfo-editdesc-ok" class="ui-btn ui-corner-all ui-shadow"><?php $locale->write("save"); ?></button>
+			</div>
+		</div>
+
+		<!-- Popup: Modify start time, end time, type, ... -->
+		<div id="challengeinfo-editetc-popup" data-role="popup" data-theme="a" data-position-to="window" class="ui-corner-all">
+			<div data-role="header" data-theme="b">
+				<h3><?php $locale->write("challenge.info.edit.title"); ?></h3>
+			</div>
+
+			<div role="main" class="ui-content">
+
+<?php
+				InputTemplates::printDropDown("challengeinfo-edit-type", $locale->get("challenge.create.type"), false, array(
+							$locale->get("challenge.create.type.default") => "0", $locale->get("challenge.create.type.ctf") => "1"
+					), "default");
+?>
+
+				<div class="ui-field-contain">
+					<label for="challengeinfo-edit-starttime"><?php $locale->write("challenge.create.start_time"); ?></label>
+					<input id="challengeinfo-edit-starttime" type="datetime-local" placeholder="<?php print(date("Y-m-d h:m:s"))?>">
+				</div>
+
+				<div class="ui-field-contain">
+					<label for="challengeinfo-edit-endtime"><?php $locale->write("challenge.create.end_time"); ?></label>
+					<input id="challengeinfo-edit-endtime" type="datetime-local" placeholder="(Optional)">
+				</div>
+
+				<label>
+			        <input id="challengeinfo-edit-predefteams" type="checkbox"><?php $locale->write("challenge.create.predef_teams"); ?>
+			    </label>
+
+<?php
+				$values1 = array($locale->get("challenge.info.edit.unlimited_teams") => -1);
+				$values2 = array();
+				for($i = 1; $i < 10; $i++){
+					$values1[$i] = $i;
+					$values2[$i] = $i;
+				}
+
+				InputTemplates::printDropDown("challengeinfo-edit-maxteams", $locale->get("challenge.create.max_team_cnt"), false, $values1, 4);
+				InputTemplates::printDropDown("challengeinfo-edit-maxteam-members", $locale->get("challenge.create.max_team_members"), false, $values2, 4);
+?>
+				<hr>
+				<button id="challengeinfo-editetc-ok" class="ui-btn ui-corner-all ui-shadow"><?php $locale->write("save"); ?></button>
 			</div>
 		</div>
 	</div>

b4676057314d1c88128fe6e9b91c22c8d8f3d696
Raphael Harzer <raphael.harzer@mni.thm.de>
2016-03-29 00:43:43 +0200

    fix bug install database

--- install/sql/generic.default_data.sql
+++ install/sql/generic.default_data.sql
@@ -9,4 +9,4 @@ INSERT INTO AccountType (acc_type_id, name) VALUES (2, 'facebook');
 INSERT INTO ChallengeType (challenge_type_id, acronym, full_name) VALUES (0, 'Race', 'Default Challenge');
 INSERT INTO ChallengeType (challenge_type_id, acronym, full_name) VALUES (1, 'CTF', 'Capture the Flag');
 
-INSERT INTO GeoCat(db_version, db_revision) VALUES ("%DB_VERSION%", %DB_REVISION%);
+INSERT INTO GeoCat(db_version, db_revision) VALUES ('%DB_VERSION%', %DB_REVISION%);

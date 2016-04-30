73efe0850a8536de0f65dbfdde810865bafc4ed1
Bastian Kraemer <Bastian.Kraemer@akubix.de>
2016-04-30 17:56:52 +0200

    Fix grunt build configuration

--- Gruntfile.js
+++ Gruntfile.js
@@ -41,6 +41,7 @@ module.exports = function(grunt) {
 						[
 							'src/js/gpsnavigator/GPSNavigationController.js',
 							'src/js/controller/*.js',
+							'src/js/Account.js',
 							'src/js/challenges/*.js'
 						]
 				}
